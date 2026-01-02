adb274b8d3c85b70d444736637acd23bcb4667ae
Jonas Gakpo <jonas.gakpo@tech11.com>
2026-01-02 00:29:43 +0000

    Verify invalid credential for login

--- tests/loginPage.spec.js
+++ tests/loginPage.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('Verify Successful Login with Valid Credentials', async ({ page }) => {
   await page.goto('https://practice.expandtesting.com/login');
 
   let username = page.locator('#username');
@@ -17,6 +17,20 @@ test('has title', async ({ page }) => {
 
   await expect(page.locator('a[href="/logout"]')).toBeVisible();
 
+});
+
+test('Verify Login Failure with Invalid Username', async ({ page }) => {
+  await page.goto('https://practice.expandtesting.com/login');
+
+  let username = page.locator('#username');
+  let password = page.locator('#password');
+
+  await username.fill('invaliduser123');
+  await password.fill('SuperSecretPassword!');
+
+  await page.locator('#submit-login').click();
+
+  await expect(page.locator('#flash b')).toHaveText('Your username is invalid!');
 });
 
 
