8c471f196c782dfae1c7b2349e89a3e308dfb14a
Jonas Gakpo <jonas.gakpo@tech11.com>
2026-01-05 11:24:37 +0000

    Refactoring

--- pages/SecureAreaPage.js
+++ pages/SecureAreaPage.js
@@ -2,5 +2,6 @@ export class SecureAreaPage{
     constructor(page){
         this.page = page;
         this.displayedText = page.locator('#flash b');
+        this.logoutBtn = page.locator('a[href="/logout"]');
     }
 }
